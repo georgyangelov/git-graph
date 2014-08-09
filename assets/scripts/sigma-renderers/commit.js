f6aad5d62b2a74d204d6e5eaf19fffdc456c387c
Georgy Angelov <georgyangelov@gmail.com>
2014-08-10 00:45:30 +0300

    Show commit message when zoomed in

--- assets/scripts/sigma-renderers/commit.js
+++ assets/scripts/sigma-renderers/commit.js
@@ -11,7 +11,11 @@
 
         fontFamily: 'Arial',
         centerTextSize: 0.3,
-        sha1Cutoff: 7
+        messageTextSize: 0.1,
+        messageTextLineHeight: 1.3,
+        sha1Cutoff: 7,
+
+        detailsSize: 70
     };
 
     window.sigma.canvas.nodes.commit = function(node, context, settings) {
@@ -23,19 +27,21 @@
             height  = size,
             padding = config.padding * size;
 
-        if (node.data.parents.length > 1) {
-            context.fillStyle = config.mergeColor;
-        } else {
-            context.fillStyle = config.normalColor;
-        }
-
+        context.fillStyle = config.normalColor;
         context.strokeStyle = config.strokeColor;
         context.setLineWidth(config.strokeSize * size);
 
-        box();
-        text_field(node.data.sha1, x, y, width - padding * 2);
+        if (size < config.detailsSize) {
+            small_box();
+        } else {
+            big_box();
+        }
 
-        function box() {
+        function small_box() {
+            if (node.data.parents.length > 1) {
+                context.fillStyle = config.mergeColor;
+            }
+
             context.beginPath();
             context.rect(
                 x - width / 2,
@@ -47,15 +53,84 @@
 
             context.stroke();
             context.fill();
+
+            text_field([node.data.sha1.substring(0, config.sha1Cutoff)], config.centerTextSize * size, x, y, width - padding * 2);
         }
 
-        function text_field(text, x, y, w) {
-            context.font = (config.centerTextSize * size) + 'pt ' + config.fontFamily;
+        function big_box() {
+            if (node.data.parents.length > 1) {
+                context.strokeStyle = config.mergeColor;
+            }
+
+            context.beginPath();
+            context.rect(
+                x - width / 2,
+                y - height / 2,
+                width,
+                height
+            );
+            context.closePath();
+
+            context.stroke();
+            context.fill();
+
+            if (!node.fragmentMessage) {
+                node.fragmentMessage = fragmentText(node.data.message, width - padding * 2);
+            }
+
+            text_field(node.fragmentMessage, config.messageTextSize * size, x, y, width - padding * 2);
+        }
+
+        function text_field(lines, size, x, y, w) {
+            context.font = size + 'pt ' + config.fontFamily;
             context.fillStyle = '#000';
             context.textAlign = 'center';
-            context.textBaseline = 'middle';
 
-            context.fillText(text.substring(0, config.sha1Cutoff), x, y);
+            var height = lines.length * size * config.messageTextLineHeight;
+
+            lines.forEach(function(line, i) {
+                context.fillText(line, x, y - height / 2 + (i + 1) * size * config.messageTextLineHeight);
+            });
+        }
+
+        // http://stackoverflow.com/questions/2936112/text-wrap-in-a-canvas-element
+        function fragmentText(text, maxWidth) {
+            var words = text.split(' '),
+                lines = [],
+                line = "";
+
+            if (context.measureText(text).width < maxWidth) {
+                return [text];
+            }
+
+            while (words.length > 0) {
+                var split = false;
+
+                while (context.measureText(words[0]).width >= maxWidth) {
+                    var tmp = words[0];
+                    words[0] = tmp.slice(0, -1);
+
+                    if (!split) {
+                        split = true;
+                        words.splice(1, 0, tmp.slice(-1));
+                    } else {
+                        words[1] = tmp.slice(-1) + words[1];
+                    }
+                }
+
+                if (context.measureText(line + words[0]).width < maxWidth) {
+                    line += words.shift() + " ";
+                } else {
+                    lines.push(line);
+                    line = "";
+                }
+
+                if (words.length === 0) {
+                    lines.push(line);
+                }
+            }
+
+            return lines;
         }
     };
 
